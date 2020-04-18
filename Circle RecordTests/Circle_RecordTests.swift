fe9181e9ea3b673fa4e0a48f843236ec5f97af89
wfordev <wfordev@gamil.com>
2020-04-19 01:03:16 +0900

    first

---
+++ Circle RecordTests/Circle_RecordTests.swift
@@ -0,0 +1,34 @@
+//
+//  Circle_RecordTests.swift
+//  Circle RecordTests
+//
+//  Created by watabe on 2020/04/19.
+//  Copyright © 2020 watabe. All rights reserved.
+//
+
+import XCTest
+@testable import Circle_Record
+
+class Circle_RecordTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
